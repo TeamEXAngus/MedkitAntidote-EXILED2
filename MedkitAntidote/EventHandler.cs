92e329c2ace3363624cc03794d35f34150785592
Marco15453 <marcolpder@gmail.com>
2021-10-09 12:32:52 +0200

    Parabellum Release

---
+++ MedkitAntidote/EventHandler.cs
@@ -0,0 +1,47 @@
+﻿using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.Events.EventArgs;
+using System.Collections.Generic;
+
+namespace MedkitAntidote
+{
+    internal sealed class EventHandler
+    {
+        public MedkitAntidote plugin;
+        public EventHandler(MedkitAntidote plugin) => this.plugin = plugin;
+
+        public void OnUsedItem(UsedItemEventArgs ev)
+        {
+            Log.Debug($"{ev.Player.Nickname} used an {ev.Item}.",plugin.Config.DebugEnabled);
+
+            //Store the appropriate list from the config file to a temporary variable
+            List<EffectType> effectList = new List<EffectType> { };
+            switch (ev.Item.Type)
+            {
+                case ItemType.Medkit:
+                    effectList = plugin.Config.MedkitEffectsToRemove;
+                    break;
+
+                case ItemType.Adrenaline:
+                    effectList = plugin.Config.AdrenalineEffectsToRemove;
+                    break;
+
+                case ItemType.Painkillers:
+                    effectList = plugin.Config.PillsEffectsToRemove;
+                    break;
+            }
+
+            //Loop through the list from the config file and remove chosen effects from a player after they've used a medical item
+            if (effectList.Count > 0)
+            {
+                foreach (EffectType effectName in effectList)
+                {
+                    //Debug message
+                    Log.Debug($"Trying to remove {effectName} from {ev.Player.Nickname}", plugin.Config.DebugEnabled);
+
+                    ev.Player.DisableEffect(effectName);
+                }
+            }
+        }
+    }
+}
--- MedkitAntidote/MedkitAntidote.cs
+++ MedkitAntidote/MedkitAntidote.cs
@@ -8,47 +8,42 @@ namespace MedkitAntidote
 {
     public class MedkitAntidote : Plugin<Config>
     {
-        private static MedkitAntidote singleton = new MedkitAntidote();
-        public static MedkitAntidote Instance => singleton;
-        public override PluginPriority Priority { get; } = PluginPriority.Medium;
-
-        public override Version RequiredExiledVersion { get; } = new Version(2, 10, 0);
-        public override Version Version { get; } = new Version(1, 0, 1);
+        public override string Name => "Medkit Antidote";
+        public override string Author => "TeamEXAngus";
+        public override Version Version => new Version(1, 1, 0);
+        public override Version RequiredExiledVersion => new Version(3, 0, 4);
 
         //Event handler for player events
-        private Handlers.Player player;
-
-        //Copied from tutorial
-        private MedkitAntidote()
-        {
-        }
+        private EventHandler eventHandler;
 
         //Run startup code when plugin is enabled
         public override void OnEnabled()
         {
             RegisterEvents();
+            base.OnEnabled();
         }
 
         //Run shutdown code when plugin is disabled
         public override void OnDisabled()
         {
             UnregisterEvents();
+            base.OnDisabled();
         }
 
         //Plugin startup code
         public void RegisterEvents()
         {
-            player = new Handlers.Player();
+            eventHandler = new EventHandler(this);
 
-            Player.MedicalItemUsed += player.OnMedicalItemUsed;
+            Player.UsingItem += eventHandler.OnUsedItem;
         }
 
         //Plugin shutdown code
         public void UnregisterEvents()
         {
-            Player.MedicalItemUsed -= player.OnMedicalItemUsed;
+            Player.UsingItem -= eventHandler.OnUsedItem;
 
-            player = null;
+            eventHandler = null;
         }
     }
 }
